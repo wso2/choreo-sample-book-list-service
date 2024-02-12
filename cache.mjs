c511ec2b0ab850bd2bf3f9d51aaa87f0a2e1ae7a
Thushara Sampath <mkt.sampath@yahoo.com>
2024-02-12 10:44:37 +0530

    Add Initial Data to the cache

--- cache.mjs
+++ cache.mjs
@@ -1,10 +1,36 @@
-import NodeCache from "node-cache";
+import NodeCache from 'node-cache';
+import { v4 as uuidv4 } from 'uuid';
 
 const cache = new NodeCache({
   stdTTL: 0,
   useClones: false,
   deleteOnExpire: true,
   maxKeys: 100,
+});
+
+const initialData = [
+  {
+    uuid: uuidv4(),
+    title: 'The Fellowship of the Ring',
+    author: 'J.R.R. Tolkien',
+    status: 'read',
+  },
+  {
+    uuid: uuidv4(),
+    title: 'The Two Towers',
+    author: 'J.R.R. Tolkien',
+    status: 'reading',
+  },
+  {
+    uuid: uuidv4(),
+    title: 'The Return of the King',
+    author: 'J.R.R. Tolkien',
+    status: 'to_read',
+  },
+];
+
+initialData.forEach((book) => {
+  cache.set(book.uuid, book);
 });
 
 export default cache;
